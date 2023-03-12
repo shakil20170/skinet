b50d5925584d55bd6237ffddd001124c7a757cb5
Shakil Sunmoon <sshakilsultan74@gmail.com>
2023-03-12 19:34:43 +0600

    test purpose

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -29,7 +29,7 @@ namespace Infrastructure.Data
             return await _context.Products
             .Include(p=>p.ProductBrand)
             .Include(p=>p.ProductType)
-            .ToListAsync();
+            .ToListAsync();;
         }
         //Product
 
